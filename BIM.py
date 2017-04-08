abc6dc658cf14b38a6bedbe56c8fed96fad95143
chenzhen2181 <120332986@qq.com>
2017-04-08 23:35:39 +0800

    BMI -0.2

--- BIM.py
+++ BIM.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
-height=input('身高（m）：')
-weight=input('体重（kg）：')
-BMI=float(weight)/float(height)**2
-BMI_ref = ('偏瘦','正常','偏胖','肥胖','重度肥胖','极重度肥胖')
-BMI_data=(18.5,24,27,30,40)
-i=0
-for n in BMI_ref:
-    if BMI<BMI_data[0]:
-        print('BMI=%.2f,' % BMI + "您体型（中国标准）"+ str(BMI_ref[0]))
-        break
-    if BMI_data[i-1]<BMI<BMI_data[i]:
-        print('BMI=%.2f,' %BMI + "您体型（中国标准）"+str(BMI_ref[i]) )
-        break
-    if BMI > BMI_data[4]:
-        print('BMI=%.2f,'  % BMI + "您体型（中国标准）"+ str(BMI_ref[5]))
-        break
-    i=i+1
+height = input('身高（m）：')
+weight = input('体重（kg）：')
+BMI = float(weight) / float(height) ** 2
+BMI_ref = ('偏瘦', '正常', '偏胖', '肥胖', '重度肥胖', '极重度肥胖')
+BMI_data = (18.5, 24, 27, 30, 40)
+strr ="您体型（中国标准）属于"
+for i in range(0, len(BMI_ref) - 1):
+	if BMI < BMI_data[0]:
+		print('BMI=%.2f,' % BMI + strr + str(BMI_ref[0]))
+		break
+	if BMI_data[i - 1] < BMI < BMI_data[i]:
+		print('BMI=%.2f,' % BMI + strr  + str(BMI_ref[i]))
+		break
+	if BMI > BMI_data[4]:
+		print('BMI=%.2f,' % BMI + strr  + str(BMI_ref[5]))
+		break
 print('BMI=%.2f' % BMI)
-
-
--- learnpython.py
+++ learnpython.py
@@ -1,1 +1,17 @@
 print("hello, world")
+n = 'abc'
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+list1=[n,f,s1,s2,s3,s4]
+#for n in list1:
+#    print(n)
+#print(n.encode('ascii'))
+a=75
+b=85
+r=b/a-1
+#print("小米,%.1f" % r)
+
