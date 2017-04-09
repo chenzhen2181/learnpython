239867426b04fbfefcc1b31002b782fa5bfc460c
chenzhen2181 <120332986@qq.com>
2017-04-09 12:31:37 +0800

    BMI -0.31 BMI 函数

--- BIM.py
+++ BIM.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-
-
 def BMI_CAL(height, weight):
 	BMI = float(weight) / float(height) ** 2
 	BMI_ref = ('偏瘦', '正常', '偏胖', '肥胖', '重度肥胖', '极重度肥胖')
@@ -9,11 +6,16 @@ def BMI_CAL(height, weight):
 	strr = "您体型（中国标准）属于:"
 	for i in range(0, len(BMI_ref) - 1):
 		if BMI < BMI_data[0]:
-			return print('BMI=%.2f,' % BMI + strr + str(BMI_ref[0]))
+			print('BMI=%.2f,' % BMI + strr + str(BMI_ref[0]))
+			break
 		if BMI_data[i - 1] < BMI < BMI_data[i]:
-			return print('BMI=%.2f,' % BMI + strr + str(BMI_ref[i]))
+			print('BMI=%.2f,' % BMI + strr + str(BMI_ref[i]))
+			break
 		if BMI > BMI_data[4]:
-			return print('BMI=%.2f,' % BMI + strr + str(BMI_ref[5]))
+			print('BMI=%.2f,' % BMI + strr + str(BMI_ref[5]))
+			break
+
+
 height = input('身高（m）：')
 weight = input('体重（kg）：')
 BMI_CAL(height, weight)
